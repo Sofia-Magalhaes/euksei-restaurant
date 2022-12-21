e7b99a9f88804c5744b1aa11870bbec09e73df6d
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2022-12-21 19:58:10 -0300

    Navbar
    navbar finalizada, logo mudada para euksei

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { GiHamburgerMenu, GIHamburgerMenu } from 'react-icons/gi';
+import { MdOutlineRestaurantMenu } from 'react-icons/md';
 
+import images from '../../constants/images';
 import './Navbar.css';
 
-const Navbar = () => (
-  <div>
-    Navbar
-  </div>
-);
+const Menu = () => (
+  <>
+    <li className='p__opensans'><a href="#home">Home</a></li>
+    <li className='p__opensans'><a href="#sobre">Sobre</a></li>
+    <li className='p__opensans'><a href="#menu">Menu</a></li>
+    <li className='p__opensans'><a href="#premios">Prêmios</a></li>
+    <li className='p__opensans'><a href="#contato">Contato</a></li>
+  </>
+)
+
+const Navbar = () => {
+
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  return (
+    <nav className='app__navbar'>
+      <div className='app__navbar-logo'>
+        <img src={images.gericht} alt="app logo" />
+      </div>
+      <ul className='app__navbar-links'>
+        <Menu />
+      </ul>
+      <div className='app__navbar-login'>
+        <a href="#login" className='p__opensans'>Entrar / Cadastrar</a>
+        <div />
+        <a href="/" className='p__opensans'>Reservar Mesa</a>
+      </div>
+      <div className='app__navbar-smallscreen'>
+        <GiHamburgerMenu color='#fff' fontSize={27} onClick={() => setToggleMenu(true)} className='overlay__open' />
+        
+        {toggleMenu && (
+          <div className='app__navbar-smallscreen_overlay flex__center slide-bottom'>
+            <MdOutlineRestaurantMenu fontSize={27} className='overlay__close' onClick={() => setToggleMenu(false)} />
+            <ul className='app__navbar-smallscreen_links'>
+              <Menu />
+            </ul>
+          </div>
+        )}
+
+      </div>
+    </nav>
+  )
+}
 
 export default Navbar;
